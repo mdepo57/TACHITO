a6061494c20fd29fcab6ef1dcfa814b7cb4cb5a8
mdepo57 <meditaciondeportiva@gmail.com>
2025-05-22 11:05:18 +0200

    Delete game.js

--- game.js
+++
@@ -1,4 +0,0 @@
-// Lógica base del juego
-console.log('Juego iniciado');
-const jugador = localStorage.getItem("playerName") || "Jugador";
-document.getElementById("mesa").innerHTML = `Jugador: ${jugador}`;
