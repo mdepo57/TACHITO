098c95a74b4b6e8e312fc2900517bc7d6b3c8e39
mdepo57 <meditaciondeportiva@gmail.com>
2025-05-22 10:55:44 +0200

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('startGame').addEventListener('click', () => {
-  const name = document.getElementById('playerName').value;
-  const mode = document.getElementById('modeSelect').value;
-  const difficulty = document.getElementById('botDifficulty').value;
-  localStorage.setItem('playerName', name);
-  localStorage.setItem('mode', mode);
-  localStorage.setItem('difficulty', difficulty);
-  window.location.href = 'game.html';
-});
