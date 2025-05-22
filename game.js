e9f87ed1ca5d550e2be904514c43a19bdb263b84
mdepo57 <meditaciondeportiva@gmail.com>
2025-05-22 21:03:08 +0200

    Delete game.js

--- game.js
+++
@@ -1,4 +0,0 @@
-const nombres = JSON.parse(localStorage.getItem("nombres"));
-const dificultades = JSON.parse(localStorage.getItem("dificultades"));
-iniciarPartida(nombres, dificultades);
-document.getElementById("hud").innerText = "Turno actual: " + nombres[turnoActual];
